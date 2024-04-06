04b193f19e6e647d4211c9b663a1c72dfdbfda4e
algoalt <166248490+algoalt@users.noreply.github.com>
2024-04-06 22:02:39 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import openai
 from .utils import get_static_file, throw_if_missing
 import os
-
+import google.generativeai as genai
 
 def main(context):
     throw_if_missing(os.environ, ["OPENAI_API_KEY"])
@@ -20,16 +20,19 @@ def main(context):
     except ValueError as err:
         return context.res.json({"ok": False, "error": err.message}, 400)
 
-    openai.api_key = os.environ["OPENAI_API_KEY"]
+    gemini_key = os.environ["OPENAI_API_KEY"]
 
     try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            max_tokens=int(os.environ.get("OPENAI_MAX_TOKENS", "512")),
-            messages=[{"role": "user", "content": context.req.body["prompt"]}],
-        )
-        completion = response.choices[0].message.content
-        return context.res.json({"ok": True, "completion": completion}, 200)
+        #response = openai.ChatCompletion.create(
+        #    model="gpt-3.5-turbo",
+        #    max_tokens=int(os.environ.get("OPENAI_MAX_TOKENS", "512")),
+        #    messages=[{"role": "user", "content": context.req.body["prompt"]}],
+        #)
+        #completion = response.choices[0].message.content
+        genai.configure(api_key = gemini_key)
+        gemini_model = genai.GenerativeModel("gemini-pro")
+        chat = gemini_model.generate_content("who is Jesus")
+        return context.res.json({"ok": True, "completion": chat.text}, 200)
 
     except Exception:
         return context.res.json({"ok": False, "error": "Failed to query model."}, 500)
